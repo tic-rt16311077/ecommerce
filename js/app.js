abac84ecd25d154bf70d4b50057d5ca94d788fc9
Rolando Muñoz Padilla <tic-rt16311077@uthermosillo.edu.mx>
2021-10-22 11:48:27 -0700

    master:Proyecto con archivo manifest.json y serviceworker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
-if(navigator.serviceWorker){
-    console.log("Se encontro ServiceWorker");
-    navigator.serviceWorker.register('/sw.js');
-}else{
-    console.log("No se encontro SW");
-}
+//if(navigator.serviceWorker){
+//    console.log("Se encontro ServiceWorker");
+//    navigator.serviceWorker.register('/sw.js');
+//}else{
+//    console.log("No se encontro SW");
+//}
+
+/*if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.register('/sw.js', {
+        scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function(registration) {
+        // Registration was successful
+        console.log('El serviceWorker se Registro Satisfactoriamente son el scope: ', registration.scope);
+    }, function(err) {
+        // registration failed :(
+        console.log('Fallo al Registrar el ServiceWorker: ', err);
+    });
+}*/
+
+if (navigator.serviceWorker.controller) {
+    console.log("Se Encontro un ServiceWorker");
+    } 
+    else
+    {
+        navigator.serviceWorker.register("/sw.js", {
+        scope: "./"
+        })
+        .then(function (reg) {
+        console.log("El serviceWorker se Registro Satisfactoriamente");
+    });
+};
--- sw.js
+++ sw.js
@@ -1,7 +1,66 @@
-self.addEventListener('install', event=>{
-    console.log('Se instala SW... Nuevo');
+//Con esta opcion se habilita la opcion de hacer la PWA instalable
+self.addEventListener('fetch', event =>{});
+
+//Se instala el ServiceWorker y se configura el cache para trabajar fuera de linea
+this.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/index.html',
+          '/carrito.html',
+          '/css/estilos.css',
+          '/js/app.js',
+          '/js/jquery.js',
+          '/images/logo1.jpg',
+          '/images/moto.jpg',
+          '/images/samsung.jpg',
+          '/images/huawei.jpg',
+          '/images/iphone.jpg',
+          '/images/banner1.jpg',
+          '/images/banner2.jpg',
+          '/images/banner3.jpg',
+          '/images/banner4.jpg',
+          
+        ]);
+      })
+    );
+  });
+
+
+//Rutina para activar el serviceWorker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(keys => Promise.all(
+        keys.map(key => {
+          if (!expectedCaches.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )).then(() => {
+               // caché de control V2
+      })
+    );
+  });
+
+//Rutina de notificaciones
+//Pedir permiso para ejecutar notificaciones
+self.addEventListener('push', function(e) {
+  const message = e.data.json(); // 1
+
+  const options = { // 2
+    body: message.body,
+    data: 'http://localhost:8080',
+    actions: [
+      {
+        action: 'Detail',
+        title: 'Detalles'
+      }
+    ]
+  };
+
+  e.waitUntil(self.registration.showNotification(message.title, options)); // 3
 });
 
-self.addEventListener('activate', event =>{
-    console.log('el SW se ha activado');
-});
+
+  
+  
